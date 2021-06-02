298432fc97ec2622a5fc2bb5e4524393a0697ed1
Kenny Uriarte Cortegana <kennyuriarte@gmail.com>
2021-06-02 12:57:13 -0500

    Add files via upload

---
+++ FactorizarUnNúmero.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	int n, i;
+	cout << "Ingrese numero:";
+	cin >> n;
+	i=2;
+	while ( i<=0 )
+	{
+		if ( n%i==0)
+		    {
+			    n = n / i;
+				cout << i << endl;
+		    }
+		else i =i + 1;
+	}
+	return 0;
+}
+
